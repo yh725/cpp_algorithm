5fae2be14cc1b569c9036494af47d877c1463dc2
yh725 <yh725@kakao.com>
2020-10-15 22:23:39 +0900

    1

---
+++ cpp001.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	int N;
+	int M;
+	int sum = 0;
+	
+	cin >> N >> M;
+	
+	for(int i=1; i <=N ;i++){
+		if(i%M==0){
+			sum += i;
+		}
+	}
+	
+	cout << sum << endl;
+	
+	return 0;
+}
