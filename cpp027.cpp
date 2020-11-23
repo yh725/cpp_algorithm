b18397a3331ed393a90e967a649da074e807fcf7
yh725 <yh725@kakao.com>
2020-11-23 23:11:21 +0900

    ca 15

---
+++ cpp027.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n, i, j, tmp;
+	
+	scanf("%d", &n);
+	
+	vector<int> ch(n+1);
+	
+	// 소인수 분해 
+	for(i=2; i<=n; i++){
+		tmp=i;
+		j=2;
+		while(1){
+			if(tmp%j==0){
+				tmp=tmp/j;
+				ch[j]++;
+			}else{
+				j++;
+			}
+			if(tmp==1) break;
+		}
+	}
+	
+	for(i=2; i<=n; i++){
+		if(ch[i]!=0) printf("%d ", ch[i]);
+	}
+	
+	return 0;
+}
